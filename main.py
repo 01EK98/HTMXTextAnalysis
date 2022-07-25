38383a5a112cfa90f903e5283ef8cfc64d2782d7
aleks <aleksander.koblass@gmail.com>
2022-07-25 02:29:50 +0200

    Generate wordcloud

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import asyncio
-from re import template
+from nltk import tokenize
+from textblob import TextBlob
+from wordcloud import WordCloud, STOPWORDS
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
-from nltk.sentiment import SentimentIntensityAnalyzer
 
 
 app = FastAPI()
@@ -20,11 +20,27 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/individual_sentiment", response_class=HTMLResponse)
-async def individual_sentiment(request: Request, sentence: str):
-    analyzer = SentimentIntensityAnalyzer()
-    sentiment = analyzer.polarity_scores(sentence)
+@app.get("/sentiment", response_class=HTMLResponse)
+async def sentiment(hx_request: Request, text_for_analysis: str):
+    sentences = tokenize.sent_tokenize(text_for_analysis)
+
     return templates.TemplateResponse(
-        "fragments/individual_sentiment.html",
-        {"request": request, "sentiment": sentiment},
+        "fragments/sentiment.html",
+        {
+            "request": hx_request,
+            "sentiments": [TextBlob(sentence).sentiment for sentence in sentences],
+        },
     )
+
+
+@app.get("/wordcloud", response_class=HTMLResponse)
+async def wordcloud(hx_request: Request, text_for_analysis: str):
+    wordcloud = WordCloud(
+        min_font_size=10,
+        background_color="white",
+        width=200,
+        height=200,
+        stopwords=set(STOPWORDS),
+    ).generate(text_for_analysis)
+
+    return HTMLResponse(wordcloud.to_svg())
