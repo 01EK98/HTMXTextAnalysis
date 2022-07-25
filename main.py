02ab69b47aeded3a949fb008bf6aa4d48f64f925
aleks <aleksander.koblass@gmail.com>
2022-07-26 00:39:07 +0200

    add sentiment bars per sentence

--- main.py
+++ main.py
@@ -16,31 +16,41 @@ templates = Jinja2Templates(directory="./")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/sentiment", response_class=HTMLResponse)
 async def sentiment(hx_request: Request, text_for_analysis: str):
     sentences = tokenize.sent_tokenize(text_for_analysis)
+    adjusted_sentiments = [
+        (TextBlob(sentence).sentiment.polarity + 1) / 2 for sentence in sentences
+    ]
 
     return templates.TemplateResponse(
         "fragments/sentiment.html",
         {
             "request": hx_request,
-            "sentiments": [TextBlob(sentence).sentiment for sentence in sentences],
+            "sentiments": adjusted_sentiments,
         },
     )
 
 
 @app.get("/wordcloud", response_class=HTMLResponse)
 async def wordcloud(hx_request: Request, text_for_analysis: str):
-    wordcloud = WordCloud(
-        min_font_size=10,
-        background_color="white",
-        width=200,
-        height=200,
-        stopwords=set(STOPWORDS),
-    ).generate(text_for_analysis)
+    try:
+        wordcloud = WordCloud(
+            min_font_size=10,
+            background_color="black",
+            width=300,
+            height=300,
+            stopwords=set(STOPWORDS),
+            font_path="./assets/Oswald-Bold.ttf",
+        ).generate(text_for_analysis)
+    except ValueError as error:
+        return templates.TemplateResponse(
+            "fragments/wordcloud_error.html",
+            {"request": hx_request, "error": error.args[0]},
+        )
 
     return HTMLResponse(wordcloud.to_svg())
