90651e00267f48d901acf2f015524a880c7b623b
aleks <aleksander.koblass@gmail.com>
2022-08-17 03:01:21 +0200

    Add another func test and a file doc

--- tests/functional_tests/functional_tests.py
+++ tests/functional_tests/functional_tests.py
@@ -1,3 +1,13 @@
+"""
+The tests are implemented with dependencies overridden in `test/conftest.py`,
+please keep that in mind when asserting numbers of elements and their values.
+
+In a production environment functional tests shouldn't have overridden dependencies!
+
+UI tests implemented in this manner (with overridden dependencies) can serve as 
+tests for our frontend/DOM manipulation logic though, because we don't have any 
+unit/integration tests for our HTMX and _hyperscript code.
+"""
 import pytest
 from pylenium.driver import Pylenium
 
@@ -10,10 +20,30 @@ def test_textarea_input_triggers_overall_and_individual_sentiments(
     py: Pylenium,
 ):
     py.visit(TEST_APP_URL)
-    textarea = py.get("#text_for_analysis")
+    textarea = py.get("textarea#text_for_analysis")
 
     textarea.type("test")
 
-    #  ("css_selector > *")  -> is CSS for any direct children of element with selector "css_selector"
-    assert py.get("#overall-sentiment-container > *").should().be_visible()
-    assert py.get("#sentiments").should().be_visible()
+    assert all(
+        [
+            child_element.should().be_visible
+            for child_element in py.get("#overall-sentiment-container").children()
+        ]
+    )
+    assert all(
+        [
+            child_element.should().be_visible
+            for child_element in py.get("#sentiments").children()
+        ]
+    )
+
+
+def test_clicking_get_wordcloud_button_renders_wordcloud(
+    run_server,
+    py: Pylenium,
+):
+    py.visit(TEST_APP_URL)
+
+    py.get("button#get-wordcloud").click()
+
+    assert py.get("#wordcloud").get("svg").should().be_visible()
